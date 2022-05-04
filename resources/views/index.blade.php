aac110e331be82981340191943a412107db4fc0a
Barbarubia <dnl.desimone@gmail.com>
2022-05-04 15:58:52 +0200

    Stampati dati dei film nella route /

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,19 @@
 @extends('base')
 
 @section('content')
-    <h1>My content</h1>
+    <h1>Movies</h1>
+
+    <div class="container">
+        @foreach ($movies as $movie)
+            <div class="movie-card">
+                <h3>Italian Title: {{ $movie->title }}</h3>
+                <p>Original Title: {{ $movie->original_title }}</p>
+                <p>Nationality: {{ $movie->nationality }}</p>
+                <p>Release Date: {{ $movie->date }}</p>
+                <p>Vote: {{ $movie->vote }}</p>
+
+            </div>
+        @endforeach
+    </div>
     @dd($movies)
 @endsection
