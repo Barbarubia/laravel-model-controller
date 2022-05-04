6f5c1b787d1923f921796db2a9461fec60aa5424
Barbarubia <dnl.desimone@gmail.com>
2022-05-04 18:02:57 +0200

    stile scss

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,17 +3,28 @@
 @section('title', 'Movies')
 
 @section('content')
-    <h1>Movies from Older to Newer</h1>
+    <h1 class="page-title">Movies from Older to Newer</h1>
 
     <div class="container">
         @foreach ($movies as $movie)
             <div class="movie-card">
-                <h3>Italian Title: {{ $movie->title }}</h3>
-                <p>Original Title: {{ $movie->original_title }}</p>
-                <p>Nationality: {{ $movie->nationality }}</p>
-                <p>Release Date: {{ $movie->date }}</p>
-                <p>Vote: {{ $movie->vote }}</p>
-
+                <h3 class="movie-title">{{ $movie->title }}</h3>
+                <div class="row">
+                    <div class="left-column">Original Title:</div>
+                    <div class="right-column">{{ $movie->original_title }}</div>
+                </div>
+                <div class="row">
+                    <div class="left-column">Nationality:</div>
+                    <div class="right-column">{{ $movie->nationality }}</div>
+                </div>
+                <div class="row">
+                    <div class="left-column">Release Date:</div>
+                    <div class="right-column">{{ $movie->date }}</div>
+                </div>
+                <div class="row">
+                    <div class="left-column">Vote:</div>
+                    <div class="right-column">{{ $movie->vote }}</div>
+                </div>
             </div>
         @endforeach
     </div>
